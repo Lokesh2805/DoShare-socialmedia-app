ab87e67493dee1a1049791bfab2c67f20eb66313
Lokesh <rejeevseth1971@gmail.com>
2025-04-11 00:29:33 +0530

    Create Post functionality completed

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,21 +1,98 @@
-import { useState } from "react"
+import { useMutation } from "@tanstack/react-query";
+import { ChangeEvent, useState } from "react"
+import { superbase } from "../superbase-client";
+
+interface PostInput {
+    title: string;
+    content: string;
+}
+
+
+
+const createPost = async (post: PostInput, imageFile: File) =>{
+    
+    const filePath = `${post.title}-${Date.now()}-${imageFile.name}`
+
+    const {error: uploadError} = await superbase.storage.from("posts-images").upload(filePath, imageFile)
+
+    if (uploadError) throw new Error(uploadError.message);
+
+    const {data: publicURLData} = superbase.storage.from("posts-images").getPublicUrl(filePath)
+
+    const {data, error} = await superbase.from("posts").insert({...post, image_url: publicURLData.publicUrl});
+
+    if (error) throw new Error(error.message);
+
+   return data; 
+}
 
 export const CreatePost = () => {
     const [title, setTitle] = useState<string>("");
     const [content, setContent] = useState<string>("");
+    const {mutate} = useMutation({mutationFn: (data: {post: PostInput, imageFile: File}) =>{
+       return createPost(data.post, data.imageFile); 
+    }})
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) =>{
+        if(e.target.files && e.target.files[0]){
+            setSelectedFile(e.target.files[0]);
+        }
+    }
 
     const handleSubmit = (event: React.FormEvent) =>{
         event.preventDefault();
+        if(!selectedFile) return;
+        mutate({post:{title, content}, imageFile: selectedFile});
     }
-    return <form>
-        <div>
-            <label>Title</label>
-            <input type="text" id='title' required onChange={(event)=> setTitle(event.target.value)}/>
-        </div>
-        <div>
-            <label>Content</label>
-            <textarea id='content' required rows={5} onChange={(event)=> setContent(event.target.value)}/>
-        </div>
-    <button type="submit"> Create Post</button>
-    </form>
+    return  <form onSubmit={handleSubmit} className="max-w-2xl mx-auto space-y-4">
+    <div>
+      <label htmlFor="title" className="block mb-2 font-medium">
+        Title
+      </label>
+      <input
+        type="text"
+        id="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="w-full border border-white/10 bg-transparent p-2 rounded"
+        required
+      />
+    </div>
+    <div>
+      <label htmlFor="content" className="block mb-2 font-medium">
+        Content
+      </label>
+      <textarea
+        id="content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        className="w-full border border-white/10 bg-transparent p-2 rounded"
+        rows={5}
+        required
+      />
+    </div>
+
+   
+
+    <div>
+      <label htmlFor="image" className="block mb-2 font-medium">
+        Upload Image
+      </label>
+      <input
+        type="file"
+        id="image"
+        accept="image/*"
+        onChange={handleFileChange}
+        className="w-full text-gray-200"
+      />
+    </div>
+    <button
+      type="submit"
+      className="bg-purple-500 text-white px-4 py-2 rounded cursor-pointer"
+    >
+      Create Post
+    </button>
+
+  </form>
 }
