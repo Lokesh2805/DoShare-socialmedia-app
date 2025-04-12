a0dd3287728760a613a37359ca45755ce86c8cd4
Lokesh <rejeevseth1971@gmail.com>
2025-04-12 21:09:12 +0530

    Display Post functionality completed

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,98 +1,112 @@
 import { useMutation } from "@tanstack/react-query";
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useState } from "react";
 import { superbase } from "../superbase-client";
 
 interface PostInput {
-    title: string;
-    content: string;
+  title: string;
+  content: string;
 }
 
+const createPost = async (post: PostInput, imageFile: File) => {
+  const filePath = `${post.title}-${Date.now()}-${imageFile.name}`;
 
+  const { error: uploadError } = await superbase.storage
+    .from("posts-images")
+    .upload(filePath, imageFile);
 
-const createPost = async (post: PostInput, imageFile: File) =>{
-    
-    const filePath = `${post.title}-${Date.now()}-${imageFile.name}`
+  if (uploadError) throw new Error(uploadError.message);
 
-    const {error: uploadError} = await superbase.storage.from("posts-images").upload(filePath, imageFile)
+  const { data: publicURLData } = superbase.storage
+    .from("posts-images")
+    .getPublicUrl(filePath);
 
-    if (uploadError) throw new Error(uploadError.message);
+  const { data, error } = await superbase
+    .from("posts")
+    .insert({ ...post, image_url: publicURLData.publicUrl });
 
-    const {data: publicURLData} = superbase.storage.from("posts-images").getPublicUrl(filePath)
+  if (error) throw new Error(error.message);
 
-    const {data, error} = await superbase.from("posts").insert({...post, image_url: publicURLData.publicUrl});
-
-    if (error) throw new Error(error.message);
-
-   return data; 
-}
+  return data;
+};
 
 export const CreatePost = () => {
-    const [title, setTitle] = useState<string>("");
-    const [content, setContent] = useState<string>("");
-    const {mutate} = useMutation({mutationFn: (data: {post: PostInput, imageFile: File}) =>{
-       return createPost(data.post, data.imageFile); 
-    }})
-    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const { mutate, isPending, isError } = useMutation({
+    mutationFn: (data: { post: PostInput; imageFile: File }) => {
+      return createPost(data.post, data.imageFile);
+    },
+  });
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) =>{
-        if(e.target.files && e.target.files[0]){
-            setSelectedFile(e.target.files[0]);
-        }
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setSelectedFile(e.target.files[0]);
     }
+  };
 
-    const handleSubmit = (event: React.FormEvent) =>{
-        event.preventDefault();
-        if(!selectedFile) return;
-        mutate({post:{title, content}, imageFile: selectedFile});
-    }
-    return  <form onSubmit={handleSubmit} className="max-w-2xl mx-auto space-y-4">
-    <div>
-      <label htmlFor="title" className="block mb-2 font-medium">
-        Title
-      </label>
-      <input
-        type="text"
-        id="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        className="w-full border border-white/10 bg-transparent p-2 rounded"
-        required
-      />
-    </div>
-    <div>
-      <label htmlFor="content" className="block mb-2 font-medium">
-        Content
-      </label>
-      <textarea
-        id="content"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        className="w-full border border-white/10 bg-transparent p-2 rounded"
-        rows={5}
-        required
-      />
-    </div>
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!selectedFile) return;
+    mutate({ post: { title, content }, imageFile: selectedFile });
+  };
+  return (
+    <form onSubmit={handleSubmit} className="max-w-2xl mx-auto space-y-4">
+      <div>
+        <label htmlFor="title" className="block mb-2 font-medium">
+          Title
+        </label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full border border-white/10 bg-transparent p-2 rounded"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="content" className="block mb-2 font-medium">
+          Content
+        </label>
+        <textarea
+          id="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="w-full border border-white/10 bg-transparent p-2 rounded"
+          rows={5}
+          required
+        />
+      </div>
 
-   
+      <div>
+        <label htmlFor="image" className="block mb-2 font-medium ">
+          Upload Image
+        </label>
+        <div className="relative w-full cursor-pointer">
+          <input
+            type="file"
+            id="image"
+            accept="image/*"
+            onChange={handleFileChange}
+            className="block w-full text-sm text-gray-700
+                 file:mr-4 file:py-2 file:px-4
+                 file:rounded-md file:border-0
+                 file:text-sm file:font-semibold
+                 file:bg-blue-500 file:text-white
+                 hover:file:bg-blue-600
+                 cursor-pointer"
+          />
+        </div>
+      </div>
+      <button
+        type="submit"
+        className="bg-purple-500 text-white px-4 py-2 rounded cursor-pointer"
+      >
+        {isPending ? "Creating..." : "Create Post"}
+      </button>
 
-    <div>
-      <label htmlFor="image" className="block mb-2 font-medium">
-        Upload Image
-      </label>
-      <input
-        type="file"
-        id="image"
-        accept="image/*"
-        onChange={handleFileChange}
-        className="w-full text-gray-200"
-      />
-    </div>
-    <button
-      type="submit"
-      className="bg-purple-500 text-white px-4 py-2 rounded cursor-pointer"
-    >
-      Create Post
-    </button>
-
-  </form>
-}
+      {isError && <p className="text-red-500">Error creating a post.</p>}
+    </form>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,11 @@
+import { PostLists } from "../components/PostLists"
+
 export const Home = () =>{
-    return (<div>Home</div>)
+    return ( <div className="pt-10">
+        <h2 className="text-6xl font-bold mb-6 text-center bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
+         Recent Posts</h2>
+        <div>
+            <PostLists/>
+        </div>
+    </div>)
 }
